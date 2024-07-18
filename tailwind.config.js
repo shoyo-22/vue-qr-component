ca454480d8cfd4859e66e6ede27bcba67117eae9
Murat Yessen <shoyo2209@gmail.com>
2024-07-18 10:20:05 +0500

    chore: update tailwind font config
    - Added a new font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
